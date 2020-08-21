04486426a77b0595fa76e38bf840cbde566640b1
Shivam <shivam.pansuria@gmail.com>
2020-08-20 18:32:24 -0700

    GuestBook:IP:,VisualArraySorter

--- src/intro_to_array_lists/GuestBook.java
+++ src/intro_to_array_lists/GuestBook.java
@@ -1,7 +1,28 @@
 package intro_to_array_lists;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 public class GuestBook {
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JButton button1 = new JButton();
+	JButton button2  = new JButton();
+	public void run() {
+		
+		
+	
+	
 	// Create a GUI with two buttons. One button reads "Add Name" and the other button reads "View Names". 
+frame.add(panel);
+panel.add(button1);
+panel.add(button2);
+frame.setSize(500, 100);
+frame.setVisible(true);
+button1.setText("Add Name");
+button2.setText("View Names");
+	
 	// When the add name button is clicked, display an input dialog that asks the user to enter a name. Add
 	// that name to an ArrayList. When the "View Names" button is clicked, display a message dialog that displays
 	// all the names added to the list. Format the list as follows:
@@ -9,4 +30,5 @@ public class GuestBook {
 	// Guest #2: Sandy Summers
 	// Guest #3: Greg Ganders
 	// Guest #4: Donny Doners
+	}
 }
---
+++ src/intro_to_array_lists/GuestBook_Runner.java
@@ -0,0 +1,10 @@
+package intro_to_array_lists;
+
+public class GuestBook_Runner {
+	public static void main(String[] args) {
+		 new GuestBook().run();
+	}
+
+	
+}
+
