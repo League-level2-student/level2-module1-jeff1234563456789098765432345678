0f7f508eb158abb4f13e2a26813ae51072ea5a50
Shivam <shivam.pansuria@gmail.com>
2020-08-27 18:17:56 -0700

    GuestBook,IntroToArrayLists

--- src/intro_to_array_lists/GuestBook.java
+++ src/intro_to_array_lists/GuestBook.java
@@ -1,34 +1,72 @@
 package intro_to_array_lists;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-public class GuestBook {
+public class GuestBook implements ActionListener {
 	JFrame frame = new JFrame();
 	JPanel panel = new JPanel();
 	JButton button1 = new JButton();
-	JButton button2  = new JButton();
+	JButton button2 = new JButton();
+	ArrayList<String> namesArray = new ArrayList<String>();
+	int guest = 0;
+
 	public void run() {
-		
-		
-	
-	
-	// Create a GUI with two buttons. One button reads "Add Name" and the other button reads "View Names". 
-frame.add(panel);
-panel.add(button1);
-panel.add(button2);
-frame.setSize(500, 100);
-frame.setVisible(true);
-button1.setText("Add Name");
-button2.setText("View Names");
-	
-	// When the add name button is clicked, display an input dialog that asks the user to enter a name. Add
-	// that name to an ArrayList. When the "View Names" button is clicked, display a message dialog that displays
-	// all the names added to the list. Format the list as follows:
-	// Guest #1: Bob Banders
-	// Guest #2: Sandy Summers
-	// Guest #3: Greg Ganders
-	// Guest #4: Donny Doners
+
+		// Create a GUI with two buttons. One button reads "Add Name" and the other
+		// button reads "View Names".
+		frame.add(panel);
+		panel.add(button1);
+		panel.add(button2);
+		frame.setSize(500, 100);
+		frame.setVisible(true);
+		button1.setText("Add Name");
+		button2.setText("View Names");
+		button1.addActionListener(this);
+		button2.addActionListener(this);
+		frame.setTitle("Guest Book");
+
+		// When the add name button is clicked, display an input dialog that asks the
+		// user to enter a name. Add
+		// that name to an ArrayList. When the "View Names" button is clicked, display a
+		// message dialog that displays
+		// all the names added to the list. Format the list as follows:
+		// Guest #1: Bob Banders
+		// Guest #2: Sandy Summers
+		// Guest #3: Greg Ganders
+		// Guest #4: Donny Doners
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+
+		JButton buttonClicked = (JButton) e.getSource();
+
+		if (button1 == buttonClicked) {
+
+			String Names = JOptionPane.showInputDialog("Enter a name");
+			namesArray.add(Names);
+
+		}
+		if (button2 == buttonClicked) {
+
+			String Output = "";
+			for (int i = 0; i < namesArray.size(); i++) {
+				
+				if (namesArray.get(i) != null) {
+					Output += "Guest #" + (i+1) + ": " + namesArray.get(i) + "\n";
+
+				}
+
+			}
+			JOptionPane.showMessageDialog(null, Output);
+		}
+
 	}
 }
--- src/intro_to_array_lists/IntroToArrayLists.java
+++ src/intro_to_array_lists/IntroToArrayLists.java
@@ -1,14 +1,24 @@
 package intro_to_array_lists;
 
+import java.util.ArrayList;
+
 public class IntroToArrayLists {
 	public static void main(String[] args) {
 		//1. Create an array list of Strings
 		//   Don't forget to import the ArrayList class
-		
+		ArrayList<String> array = new ArrayList<String>();
 		//2. Add five Strings to your list
+		array.add("Strings1");
+		array.add("Strings2");
+		array.add("Strings3");
+		array.add("Strings4");
 		
 		//3. Print all the Strings using a standard for-loop
-		
+		for (int i = 0; i < array.size(); i++) {
+			System.out.println(array.get(i));
+			System.out.println(array.get(i/2));
+			//System.out.println(array.get());
+		}
 		//4. Print all the Strings using a for-each loop
 		
 		//5. Print only the even numbered elements in the list.
